4cced3963e1ab7ce5b62249fcc3ce2ae74595c1c
SultanRazin <92024243+SultanF1@users.noreply.github.com>
2024-01-03 23:41:25 +0300

    remove files

--- src/types.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2019 Alexander Berezovsky
-// License: http://opensource.org/licenses/MIT
-
-export type LatLngPoint = { lat: number, lng: number }
-export type LatLngArea = { start: LatLngPoint, end: LatLngPoint }
